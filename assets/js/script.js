7aa5f11d733242b50f72b8b2caada0933b867dcb
Ranjit Patro <ranjitpatro200@gmail.com>
2023-07-22 17:06:56 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Mathematics", "Artificial Intelligence", "web Development"],
+    strings: ["Mathematics", "Artificial Intelligence", "Natural Language Processing", "Speech and Audio Processing", "AI in Healthcare", "web Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
